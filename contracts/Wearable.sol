9a34dd722faeb80c7772ce85bb58bd2a0855332e
= <=>
2022-08-13 02:07:52 -0300

    feat: add royalties

--- contracts/Wearable.sol
+++ contracts/Wearable.sol
@@ -6,14 +6,18 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 
-contract Wearable is ERC721URIStorage, Ownable, Pausable {
+contract Wearable is ERC721Royalty, ERC721URIStorage, Ownable, Pausable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Wearable", "OLA") {}
+    constructor() ERC721("Wearable", "OLA") {
+        uint96 denominator = _feeDenominator();
+        _setDefaultRoyalty(msg.sender, (denominator * 1) / 20);
+    }
 
-    function mintNFT(address recipient, string memory tokenURI)
+    function mintNFT(address recipient, string memory uri)
         public
         onlyOwner
         whenNotPaused
@@ -23,20 +27,49 @@ contract Wearable is ERC721URIStorage, Ownable, Pausable {
 
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, uri);
 
         return newItemId;
     }
 
-    function transferNFT(
+    function transferFrom(
         address from,
         address to,
         uint256 tokenId
-    ) public whenNotPaused {
-        transferFrom(from, to, tokenId);
+    ) public override whenNotPaused {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not token owner nor approved"
+        );
+        _transfer(from, to, tokenId);
     }
 
-    function destroyNFT(uint256 tokenId) public onlyOwner whenNotPaused {
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual override(ERC721) whenNotPaused {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not token owner nor approved"
+        );
+        _safeTransfer(from, to, tokenId, data);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override whenNotPaused {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not token owner nor approved"
+        );
+        _safeTransfer(from, to, tokenId, "");
+    }
+
+    function destroyNFT(uint256 tokenId) public onlyOwner {
         _burn(tokenId);
     }
 
@@ -46,5 +79,30 @@ contract Wearable is ERC721URIStorage, Ownable, Pausable {
 
     function unpause() public onlyOwner {
         _unpause();
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721Royalty, ERC721URIStorage)
+    {
+        ERC721URIStorage._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return ERC721URIStorage.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Royalty)
+        returns (bool)
+    {
+        return ERC721Royalty.supportsInterface(interfaceId);
     }
 }
